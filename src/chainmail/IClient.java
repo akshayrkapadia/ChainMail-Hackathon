55938469f95a1eeb133b8b871510f78ffb7ed7d3
Akshay R. Kapadia <akshayrkapadia@tutamail.com>
2018-10-06 11:44:32 -0400

    Merged Server and Client classes

--- src/chainmail/IClient.java
+++ src/chainmail/IClient.java
@@ -4,6 +4,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -25,11 +26,11 @@ public interface IClient {
 		}
 	}
 	
-	default Thread createClientThread() {
+	default Thread createClientThread(String ipAddress) {
 		Thread clientThread = new Thread() {
 			public void run() {
 				try {
-					Socket socket = new Socket("localHost", 9806);
+					Socket socket = new Socket(ipAddress, 9806);
 					System.out.println("Connected");
 					ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
 					Scanner scanner = new Scanner(System.in);
@@ -45,5 +46,27 @@ public interface IClient {
 			}
 		};
 		return clientThread;
+	}
+	
+	default Thread createServerThread() {
+		Thread serverThread = new Thread() {
+			public void run() {
+				try {
+					System.out.println("Waiting For Clients");
+					ServerSocket serverSocket = new ServerSocket(9806);
+					Socket socket = serverSocket.accept();
+					System.out.println("Connection Established");
+					ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+					Block inputBlock = (Block) input.readObject();
+					System.out.println("Message received: " + inputBlock.getMessage());
+					ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+					Block outputBlock = new Block(1, null, "Message Recieved", null);
+					output.writeObject(outputBlock);
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		return serverThread;
 	}
 }
--- src/chainmail/Main.java
+++ src/chainmail/Main.java
@@ -1,16 +1,16 @@
 package chainmail;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		Client client = new Client("Akshay");
-//		System.out.println(client.getName());
-//		System.out.println(client.getIPAddress());
-		Server server = new Server(client);
-//		System.out.println(server.getServerID());
 		
-		server.createServerThread().start();
-		client.createClientThread().start();
+		System.out.println(client.getIPAddress());
+		
+		client.createServerThread().start();
+		client.createClientThread("152.23.237.180").start();
 	}
 
 }
