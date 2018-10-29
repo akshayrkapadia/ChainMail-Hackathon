492efbcc50a34af12e7e789d559145f39e8b7aad
Akshay R. Kapadia <akshayrkapadia@tutamail.com>
2018-10-29 09:22:40 -0400

    Working on two way communication

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -10,12 +10,12 @@ public class Main {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Your name: ");
 		String name = in.next();
+		Client client = new Client(name);
 		System.out.println("Contact name: ");
 		String contactName = in.next();
 		System.out.println("Contact IP Address: ");
 		String contactIP = in.next();
 		in.close();
-		Client client = new Client(name);
 		Contact contact = new Contact(contactName, contactIP);
 		client.startChat(contact);
 		
